935df0c2e502b98a1320601c32b3745c70728ff3
kjyy08 <kjyy08@naver.com>
2025-10-12 18:28:24 +0900

    chore: 스프링부트 프로젝트 생성

---
+++ src/main/java/com/swygbro/airoad/backend/AiroadBackendApplication.java
@@ -0,0 +1,13 @@
+package com.swygbro.airoad.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiroadBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AiroadBackendApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/swygbro/airoad/backend/AiroadBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swygbro.airoad.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiroadBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}

46eb8fe54002403cb827a089d71c5c0b130b4dde
kjyy08 <kjyy08@naver.com>
2025-10-27 00:09:24 +0900

    refactor: main 브랜치 병합 충돌 해결

--- src/main/java/com/swygbro/airoad/backend/chat/infrastructure/AiConversationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.swygbro.airoad.backend.chat.infrastructure;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.swygbro.airoad.backend.chat.domain.entity.AiConversation;
-
-public interface AiConversationRepository extends JpaRepository<AiConversation, Long> {}
